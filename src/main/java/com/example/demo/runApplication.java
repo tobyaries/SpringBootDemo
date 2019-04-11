4cfcd99db876cba0a04222ec38617e232a826a0e
tobyaries <yestin.tian@qq.com>
2019-04-11 12:25:41 +0800

    添加上传限制

---
+++ src/main/java/com/example/demo/runApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import javax.servlet.MultipartConfigElement;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * runApplication
+ */
+@SpringBootApplication
+public class runApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(runApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize("1KB");
+        //设置上传数据总大小
+        factory.setMaxRequestSize("1024000KB");
+        return factory.createMultipartConfig();
+    }
+}
