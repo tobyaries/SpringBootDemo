72d8581248713564b505b4c26d886ec169039163
tobyaries <yestin.tian@qq.com>
2019-04-19 18:02:01 +0800

    war打包

--- src/main/java/com/example/demo/runApplication.java
+++ src/main/java/com/example/demo/runApplication.java
@@ -3,14 +3,21 @@ package com.example.demo;
 import javax.servlet.MultipartConfigElement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 /**
  * runApplication
  */
 @SpringBootApplication
-public class RunApplication {
+// public class RunApplication {
+public class RunApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RunApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RunApplication.class, args);
@@ -19,9 +26,9 @@ public class RunApplication {
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
-        //单个文件最大
+        // 单个文件最大
         factory.setMaxFileSize("100KB");
-        //设置上传数据总大小
+        // 设置上传数据总大小
         factory.setMaxRequestSize("1024000KB");
         return factory.createMultipartConfig();
     }
