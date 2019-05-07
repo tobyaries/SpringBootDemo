6ff2b9866d942f965bc6f159ddd040f5f1ba9a81
tobyaries <yestin.tian@qq.com>
2019-05-07 16:58:55 +0800

    增加rediscontroller

---
+++ src/main/java/com/example/demo/controller/RedisController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.bean.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * RedisController
+ */
+@RestController
+@RequestMapping("/api/v1/redis")
+public class RedisController {
+
+    @Autowired
+    private StringRedisTemplate redisTpl; //jdbcTemplate
+
+    @GetMapping(value = "add")
+    public Object add() {
+        redisTpl.opsForValue().set("name", "delia");
+        return JsonData.buildSuccess("OK");
+    }
+
+    @GetMapping(value = "get")
+    public Object get() {
+        String value = redisTpl.opsForValue().get("name");
+        return JsonData.buildSuccess(value);
+    }
+}
