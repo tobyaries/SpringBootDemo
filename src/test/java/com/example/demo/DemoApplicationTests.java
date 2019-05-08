5da45ba0d15f53ed1636e03e4b0d7cc1ac59b17a
tobyaries <yestin.tian@qq.com>
2019-05-08 11:41:37 +0800

    添加测试用例

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,18 +1,40 @@
 package com.example.demo;
 
-import org.junit.After;
-import org.junit.Before;
+import com.example.demo.bean.User;
+import com.example.demo.utils.JsonUtils;
+import com.example.demo.utils.RedisClient;
+
+// import org.junit.After;
+// import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import junit.framework.TestCase;
+// import junit.framework.TestCase;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {RunApplication.class})
 public class DemoApplicationTests {
+
+	@Autowired
+	private StringRedisTemplate strTpl;
+
+	@Autowired
+	private RedisClient redis;
+
 	@Test
+	public void testOne() {
+		User user = new User();
+		user.setAge(1);
+		user.setPhone("187232211111");
+		user.setName("tony");
+		String str = JsonUtils.obj2String(user);
+		redis.set("test:user:111", str);
+		System.out.println(str);
+	}
+	/* @Test
 	public void testOne() {
 		System.out.println("test hello");
 		TestCase.assertEquals(1, 1);
@@ -26,5 +48,5 @@ public class DemoApplicationTests {
 	@After
 	public void testAfter() {
 		System.out.println("after");
-	}
+	} */
 }
